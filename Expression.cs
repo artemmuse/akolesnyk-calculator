8b43ede69439ed1a4d193193be7a10be6c1beedf
artemmuse <artemuse.it@gmail.com>
2018-07-03 22:03:40 +0300

    Initial commit

---
+++ Expression.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Expression
+    {
+        private string myString;
+
+        public Expression(string myString)
+        {
+            this.myString = myString;
+        }
+         
+        public double CalculateExpressionWithoutSubExpression()
+        {
+            if (myString.Contains("+"))
+            {
+                int position = myString.LastIndexOf('+');
+                Expression expr1 = new Expression(myString.Substring(0, position));
+                Expression expr2 = new Expression(myString.Substring(position + 1, myString.Length - position - 1));
+                return expr1.CalculateExpressionWithoutSubExpression() + expr2.CalculateExpressionWithoutSubExpression();
+            }
+            else if (myString.Contains("-") && (!(myString.Contains("'-"))))
+            {
+                int position = myString.LastIndexOf('-');
+                Expression expr1 = new Expression(myString.Substring(0, position));
+                Expression expr2 = new Expression(myString.Substring(position + 1, myString.Length - position - 1));
+                return expr1.CalculateExpressionWithoutSubExpression() - expr2.CalculateExpressionWithoutSubExpression();
+            }
+            else if (myString.Contains("*"))
+            {
+                int position = myString.IndexOf('*');
+                Expression expr1 = new Expression(myString.Substring(0, position));
+                Expression expr2 = new Expression(myString.Substring(position + 1, myString.Length - position - 1));
+                return expr1.CalculateExpressionWithoutSubExpression() * expr2.CalculateExpressionWithoutSubExpression();
+            }
+            else if (myString.Contains("?"))
+            {
+                int position = myString.IndexOf('/');
+                Expression expr1 = new Expression(myString.Substring(0, position));
+                Expression expr2 = new Expression(myString.Substring(position + 1, myString.Length - position - 1));
+                return expr1.CalculateExpressionWithoutSubExpression() / expr2.CalculateExpressionWithoutSubExpression();
+            }
+            else
+            {
+                if ((myString[0] == '\'') && (myString[myString.Length-1] == '\''))
+                    return Convert.ToDouble(myString.Substring(1, myString.Length - 2));
+                else
+                    return Convert.ToDouble(myString);
+            }
+            
+        }
+
+        public Boolean ExistSubExpr() {
+            if (myString.Contains("(") && myString.Contains(")"))
+                return true;
+            else
+                return false;
+        }
+
+        public string GetSubExpr() {
+            int start = myString.IndexOf('(');
+            int stop = myString.IndexOf(')');
+            return myString.Substring(start + 1, stop - start - 1);
+        }
+
+        public double CalculateExprWithSubExpr()
+        {
+            while (ExistSubExpr())
+            {
+                Expression subExpr = new Expression(GetSubExpr());
+                string subStringNew = Convert.ToString(subExpr.CalculateExpressionWithoutSubExpression());
+                string subStringOld = "(" + subExpr.myString + ")";
+                myString = myString.Replace(subStringOld, "'"+subStringNew+"'");
+            }
+            return CalculateExpressionWithoutSubExpression();
+        }
+          
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static string myString;
+        static double answer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your line");
+            string myString = Console.ReadLine();
+            Expression expr = new Expression(myString);
+
+            answer = expr.CalculateExprWithSubExpr();
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
